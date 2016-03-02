3356dc5b272ddc3abdaf40021c22e31edff48217
Jyotirmoy Roy <posts.dir@gmail.com>
2016-03-02 17:52:37 +0600

    first commit

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -9,25 +9,37 @@
     Sara has label 0
     Chris has label 1
 """
-    
+
 import sys
 from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
 
 
-### features_train and features_test are the features for the training
-### and testing datasets, respectively
-### labels_train and labels_test are the corresponding item labels
+# features_train and features_test are the features for the training
+# and testing datasets, respectively
+# labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
-
-
 
 
 #########################################################
 ### your code goes here ###
+to = time()
+from sklearn.naive_bayes import GaussianNB
+clf = GaussianNB()
+clf.fit(features_train, labels_train)
+print "training time: ", round(time()-to, 3), "s"
 
-
+t1 = time()
+print clf.score(features_test, labels_test)
+print "prediction time: ", round(time()-t1, 3)
 #########################################################
 
+print len(features_test), len(labels_test)
 
+from sklearn import svm
+
+clf = svm.SVC(kernel="linear")
+clf.fit(features_train, labels_train)
+
+print "hi", clf.score(features_test, features_train)
--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -7,24 +7,29 @@
     Sara has label 0
     Chris has label 1
 """
-    
+
 import sys
 from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
 
 
-### features_train and features_test are the features for the training
-### and testing datasets, respectively
-### labels_train and labels_test are the corresponding item labels
+# features_train and features_test are the features for the training
+# and testing datasets, respectively
+# labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
-
-
+features_train = features_train[:len(features_train)/100]
+labels_train = labels_train[:len(labels_train)/100]
 
 #########################################################
 ### your code goes here ###
 
+from sklearn import svm
+clf = svm.SVC(kernel="rbf", C=10)
+clf.fit(features_train, labels_train)
+
+# print len(features_test), len(labels_test)
+print clf.score(features_test, labels_test)
+
 #########################################################
-
-
